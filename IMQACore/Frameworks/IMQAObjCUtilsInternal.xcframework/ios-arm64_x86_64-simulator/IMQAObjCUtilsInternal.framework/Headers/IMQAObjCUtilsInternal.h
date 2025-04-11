3844fbf6059a6fc957c65add7a975fea523a3c30
Hunta <hunta@onycom.com>
2025-04-11 11:25:08 +0900

    chore: 更新版本至 1.0.23

---
+++ IMQACore/Frameworks/IMQAObjCUtilsInternal.xcframework/ios-arm64_x86_64-simulator/IMQAObjCUtilsInternal.framework/Headers/IMQAObjCUtilsInternal.h
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2024 Embrace Mobile, Inc. All rights reserved.
+//
+
+#ifndef EmbraceObjCUtilsInternal_h
+#define EmbraceObjCUtilsInternal_h
+
+#import "IMQABinaryImageProvider.h"
+#import "IMQAStackTraceProccessor.h"
+#import "NSURLSessionTask+IMQA.h"
+#import "IMQAURLSessionDelegateForwarder.h"
+
+#endif /* EmbraceObjCUtilsInternal_h */
